74146a06408698b9641b66ca77f4ac0b86ae513a
neno <nmatosev@fesb.hr>
2017-07-26 17:50:34 +0200

    New questions

--- app/src/main/java/com/example/b312967/quizapp/DataHandler.java
+++ app/src/main/java/com/example/b312967/quizapp/DataHandler.java
@@ -24,11 +24,10 @@ public class DataHandler {
     public void addQuestions() {
         List<String> questionlist = new ArrayList<String>(); // lista pitanja i odgovora
         questionlist = ParseFile("res/raw/questions.txt");   // ime text filea
-
         String[] separated;  // array u kojem ce prvi clan bit pitanje, sljedeca 4 odgovori
 
         for (int i=0; i<questionlist.size(); i++) {
-            separated = questionlist.get(i).split(",");
+            separated = questionlist.get(i).split(";");
             try {
                 Log.w("q raw", separated[0] + separated[1] + separated[2] + separated[3] + separated[4] + separated[5]+separated[6]);
                 Category c = new Category(separated[0], separated[1], separated[2], separated[3], separated[4], separated[5], separated[6]);
@@ -36,19 +35,14 @@ public class DataHandler {
             }catch(Exception e) {
                 Log.d("Neispravan format " , questionlist.get(i));
             }
-            //dodavaj objekte u listu
         }
     }
 
 
-    public List<String> ParseFile(String filename) // filename triba bit tipa "raw/ime.text" u res/raw folderu
-    {
+    public List<String> ParseFile(String filename){
         InputStream in = this.getClass().getClassLoader().getResourceAsStream(filename);
-        Log.w("LOG", "test2");
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
         String str; // string u kojem je pitanje (linija text filea)
-        //String[] separated;  // array u kojem ce prvi clan bit pitanje, sljedeca 4 odgovori
         try {
             while ((str = reader.readLine()) != null) {
                 Log.w("dodani string", str);
