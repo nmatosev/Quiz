64e0b372aa017a435e104bea4f4db0e5d719c6f2
nmatosev <you@example.com>
2020-02-09 13:32:50 +0100

    new questions 20W7

--- app/src/main/java/com/example/b312967/quizapp/MainActivity.java
+++ app/src/main/java/com/example/b312967/quizapp/MainActivity.java
@@ -14,21 +14,32 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.HashMap;
+
 /**
  * Menu activity where user can scroll through categories.
  */
 public class MainActivity extends AppCompatActivity {
 
+    public static String[] categories = {"Povijest", "Sport", "Zemljopis", "Znanost", "Umjetnost", "Muzika", "Film"};
+    public static HashMap<Integer, Category> listViewData = new HashMap<Integer, Category>();
+
+    public static void fillData() {
+        for (int i = 0; i < categories.length; i++) {
+            Category category = new Category(i, categories[i]);
+            listViewData.put(i, category);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView myListView = (ListView) findViewById(R.id.myListView);
-        DataStorage.fillData();
-        myListView.setAdapter(new Adapter(getApplicationContext()));
+        ListView categoryListView = (ListView) findViewById(R.id.myListView);
+        fillData();
+        categoryListView.setAdapter(new Adapter(getApplicationContext()));
 
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        categoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(view.getContext(), QuizActivity.class);
                 intent.putExtra("listElement", String.valueOf(i));
